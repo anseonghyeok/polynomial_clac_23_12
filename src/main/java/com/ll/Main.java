c9aa7dda6a89f98433a84ec066e0b976f71f603d
anseonghyeok <itnew0987@gmail.com>
2023-12-29 09:08:16 +0900

    -(8+2) * -(7+3) + 5 == 105 하는중

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -5,6 +5,20 @@ public class Main {
     System.out.println("10 - 20 + 30");
     System.out.println("10 - 20 + 30".replaceAll("- ", "+ -"));
 
+    String exp = "-(8 + 2) * -(7 + 3) + 5";
+
+int startPos = 0;
+int endPos = 7;
+
+String head = exp.substring(0, startPos);
+String body = "(" + exp.substring(startPos + 1, endPos + 1) + " * -1)";
+String tail = exp.substring(endPos + 1);
+
+    System.out.println("head : " + head);
+    System.out.println("body : " + body);
+    System.out.println("tail : " + tail);
+
+    System.out.println("전체 : " + head + body + tail);
 
   }
 }
